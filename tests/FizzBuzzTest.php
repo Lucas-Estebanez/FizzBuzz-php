73cb97ed5b352fa47a1634c1a572165915052696
Lucas Estebanez <188863850+Lucas-Estebanez@users.noreply.github.com>
2026-05-26 17:58:16 +0200

    RED - Check if fizzBuzz function returns integer value

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -9,40 +9,15 @@ use PHPUnit\Framework\TestCase;
 
 final class FizzBuzzTest extends TestCase
 {
-
     /**
      * @test
      */
-    public function trueAssertion()
+    public function returnsNumberForInput()
     {
         $example = new FizzBuzz();
 
-        $integerValue = $example->integerChecker(1);
+        $integerValue = $example->fizzBuzz(1);
 
-        $this->assertTrue($integerValue);
-    }
-
-    /**
-     * @test
-     */
-    public function falseAssertion()
-    {
-        $example = new FizzBuzz();
-
-        $integerValue = $example->integerChecker('1');
-
-        $this->assertFalse($integerValue);
-    }
-
-    /**
-     * @test
-     */
-    public function equalsAssertion()
-    {
-        $example = new FizzBuzz();
-
-        $integerValue = $example->integerChecker(1);
-
-        $this->assertEquals(true, $integerValue);
+        $this->assertIsInt($integerValue);
     }
 }
